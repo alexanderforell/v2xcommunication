31947fcd7187aa9f6a939ae60082595ab0c1fdab
alexander <alexander@forell>
2014-10-20 08:23:21 +0200

    Implemented new methods to add and delete users

--- src/v2gCommunication/client/ClientMethods.java
+++ src/v2gCommunication/client/ClientMethods.java
@@ -1,35 +1,199 @@
-package v2gCommunication.client;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package v2gCommunication.client;
+import java.util.UUID;
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
 
-import java.net.*;
-import java.io.*;
 /**
  *
  * @author alexander
  */
-public class ClientMethods extends Thread{
-    Socket socket;
-    ClientMethods(Socket socket){
-        super("ClientMethods");
-        this.socket = socket;
+public class ClientMethods{
+    
+    
+    static public void logon (ServerConnection conn,String userName, String passWord){
+        UUID reQuestID = UUID.randomUUID();
+        JsonObject transmit;
+        JsonObjectBuilder message = Json.createObjectBuilder();
+        JsonArrayBuilder parameters = Json.createArrayBuilder();
+        message.add("Function-Name", "logon");
+        message.add("Request-ID", reQuestID.toString());
+        parameters.add(userName).add(passWord);       
+        message.add("Parameters",parameters);
+        transmit = message.build();
     }
-    @Override public void run() {
-        try {
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(socket.getInputStream()));
-            while ((in.readLine()) != null) {
-                System.out.println("Listening, but doint nothing.");
-            }
-        } catch (IOException ex) {
-            System.out.println("Connection to Server not possible");
-        }
-    }
-    public void sendData(String fileName, String fileType, String path, Integer packageSize){
+    static public void createUser(ServerConnection conn,String userName, String password){
+        UUID reQuestID = UUID.randomUUID();
+        JsonObject transmit;
+        JsonObjectBuilder message = Json.createObjectBuilder();
+        message.add("functionName", "createUser");
+        message.add("requestId", reQuestID.toString());
+        message.add("userName", userName);
+        message.add("password", password);
+        transmit = message.build();
+        conn.transmitData(transmit);
         
     }
+    static public void changePassword(ServerConnection conn, String userName, String passwordOld, String passwordNew){
+        UUID reQuestID = UUID.randomUUID();
+        JsonObject transmit;
+        JsonObjectBuilder message = Json.createObjectBuilder();
+        message.add("functionName", "createUser");
+        message.add("requestId", reQuestID.toString());
+        message.add("userName", userName);
+        message.add("passwordOld", passwordOld);
+        message.add("passwordNew", passwordNew);
+        transmit = message.build();
+        conn.transmitData(transmit);
+    }
+    static public void deleteUser(ServerConnection conn,String userName){
+        UUID reQuestID = UUID.randomUUID();
+        JsonObject transmit;
+        JsonObjectBuilder message = Json.createObjectBuilder();
+        message.add("functionName", "deleteUser");
+        message.add("requestId", reQuestID.toString());
+        message.add("userName", userName);
+        transmit = message.build();
+        conn.transmitData(transmit);
+    }
+    static public void logoff (ServerConnection conn, String userName){
+        UUID reQuestID = UUID.randomUUID();
+        JsonObject transmit;
+        JsonObjectBuilder message = Json.createObjectBuilder();
+        message.add("functionName", "createUser");
+        message.add("requestId", reQuestID.toString());
+        message.add("userName", userName);
+        transmit = message.build();
+        conn.transmitData(transmit);
+    }
+    static public void getUserRights(ServerConnection conn, String userName){
+        UUID reQuestID = UUID.randomUUID();
+        JsonObject transmit;
+        JsonObjectBuilder message = Json.createObjectBuilder();
+        message.add("functionName", "createUser");
+        message.add("requestId", reQuestID.toString());
+        message.add("userName", userName);
+        transmit = message.build();
+        conn.transmitData(transmit);
+    }
+    static public void addVehicle(ServerConnection conn, String vin){
+        UUID reQuestID = UUID.randomUUID();
+        JsonObject transmit;
+        JsonObjectBuilder message = Json.createObjectBuilder();
+        JsonArrayBuilder parameters = Json.createArrayBuilder();
+        message.add("Function-Name", "addVehicle");
+        message.add("Request-ID", reQuestID.toString());
+        parameters.add(vin);       
+        message.add("Parameters",parameters);
+        transmit = message.build();
+    }
+
+    static public void deleteVehicle(ServerConnection conn, String vin){
+        UUID reQuestID = UUID.randomUUID();
+        JsonObject transmit;
+        JsonObjectBuilder message = Json.createObjectBuilder();
+        JsonArrayBuilder parameters = Json.createArrayBuilder();
+        message.add("Function-Name", "addVehicle");
+        message.add("Request-ID", reQuestID.toString());
+        parameters.add(vin);       
+        message.add("Parameters",parameters);
+        transmit = message.build();
+    }
+    static public void getVehicles(ServerConnection conn){
+        UUID reQuestID = UUID.randomUUID();
+        JsonObject transmit;
+        JsonObjectBuilder message = Json.createObjectBuilder();
+        JsonArrayBuilder parameters = Json.createArrayBuilder();
+        message.add("Function-Name", "getVehicles");
+        message.add("Request-ID", reQuestID.toString());
+        parameters.add("");       
+        message.add("Parameters",parameters);
+        transmit = message.build();
+    }
+
+    static public void getActiveVehicles(ServerConnection conn){
+        UUID reQuestID = UUID.randomUUID();
+        JsonObject transmit;
+        JsonObjectBuilder message = Json.createObjectBuilder();
+        JsonArrayBuilder parameters = Json.createArrayBuilder();
+        message.add("Function-Name", "getActiveVehicles");
+        message.add("Request-ID", reQuestID.toString());
+        parameters.add("");       
+        message.add("Parameters",parameters);
+        transmit = message.build();
+            
+    }
+    static public void readData(ServerConnection conn){
+        UUID reQuestID = UUID.randomUUID();
+        JsonObject transmit;
+        JsonObjectBuilder message = Json.createObjectBuilder();
+        JsonArrayBuilder parameters = Json.createArrayBuilder();
+        message.add("Function-Name", "readData");
+        message.add("Request-ID", reQuestID.toString());
+        parameters.add("");       
+        message.add("Parameters",parameters);
+        transmit = message.build();
+
+    }
+    static public void readData(ServerConnection conn, String vin){
+        UUID reQuestID = UUID.randomUUID();
+        JsonObject transmit;
+        JsonObjectBuilder message = Json.createObjectBuilder();
+        JsonArrayBuilder parameters = Json.createArrayBuilder();
+        message.add("Function-Name", "readData");
+        message.add("Request-ID", reQuestID.toString());
+        parameters.add("vin");       
+        message.add("Parameters",parameters);
+        transmit = message.build();
+    }
+    static public void getSessions(ServerConnection conn){
+        UUID reQuestID = UUID.randomUUID();
+        JsonObject transmit;
+        JsonObjectBuilder message = Json.createObjectBuilder();
+        JsonArrayBuilder parameters = Json.createArrayBuilder();
+        message.add("Function-Name", "getSessions");
+        message.add("Request-ID", reQuestID.toString());
+        parameters.add("");       
+        message.add("Parameters",parameters);
+        transmit = message.build();
+    }
+    static public void getSessions(ServerConnection conn, String vin){
+        UUID reQuestID = UUID.randomUUID();
+        JsonObject transmit;
+        JsonObjectBuilder message = Json.createObjectBuilder();
+        JsonArrayBuilder parameters = Json.createArrayBuilder();
+        message.add("Function-Name", "getSessions");
+        message.add("Request-ID", reQuestID.toString());
+        parameters.add("vin");       
+        message.add("Parameters",parameters);
+        transmit = message.build();
+    }
+    static public void requestData(ServerConnection conn, String[] request, String vin){
+        UUID reQuestID = UUID.randomUUID();
+        JsonObject transmit;
+        JsonObjectBuilder message = Json.createObjectBuilder();
+        JsonArrayBuilder parameters = Json.createArrayBuilder();
+        message.add("Function-Name", "getSessions");
+        message.add("Request-ID", reQuestID.toString());
+        parameters.add("vin");       
+        message.add("Parameters",parameters);
+        transmit = message.build();
+    }
+    static public void requestData(ServerConnection conn, String[] request){
+        
+    }
+    static public void schduleDataRequest(ServerConnection conn, String[] request, String vin, Long intervall){
+        
+    }
+    static public void schduleDataRequest(ServerConnection conn, String[] request, Long intervall){
+        
+    }
+   
+    
 }
---
+++ src/v2gCommunication/client/ServerConnection.java
@@ -0,0 +1,46 @@
+package v2gCommunication.client;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.net.*;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.json.*;
+/**
+ *
+ * @author alexander
+ */
+public class ServerConnection extends Thread{
+    Socket socket;
+    PrintWriter out;
+    ServerConnection(Socket socket){
+        super("ServerConnection");
+        this.socket = socket;
+        try {
+            out = new PrintWriter(socket.getOutputStream(), true);
+        } catch (IOException ex) {
+            Logger.getLogger(ServerConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    @Override public void run() {
+        try {
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+            while ((in.readLine()) != null) {
+                System.out.println("Listening, but doint nothing.");
+            }
+        } catch (IOException ex) {
+            System.out.println("Connection to Server not possible");
+        }
+    }
+    public void transmitData(JsonObject data){
+        
+        
+        out.println(data.toString());
+    }
+}
