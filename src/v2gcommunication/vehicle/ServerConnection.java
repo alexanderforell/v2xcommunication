3609f788f2539d89c396ff9ffbcc7690097ec95c
alexander <alexander@forell.PE.local>
2014-11-09 17:13:07 +0100

    Cipher problem solved

--- src/v2gcommunication/vehicle/ServerConnection.java
+++ src/v2gcommunication/vehicle/ServerConnection.java
@@ -8,13 +8,15 @@ package v2gcommunication.vehicle;
 import java.io.IOException;
 import java.io.StringReader;
 import java.net.Socket;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import v2gcommunication.vehicle.interfaces.DataReceived;
 import javax.json.*;
+import v2gcommunication.commonclasses.DESEncode;
+import v2gcommunication.commonclasses.DESDecode;
+import v2gcommunication.commonclasses.ReceiveProtocol;
+import v2gcommunication.commonclasses.TransmitProtocol;
 
 
 
@@ -24,67 +26,45 @@ import javax.json.*;
  * @author alexander
  */
 public class ServerConnection extends Thread{
-    Socket socket;
+    private Socket socket;
+    private TransmitProtocol out;
+    private ReceiveProtocol in;
      List <DataReceived> listener;
      
     ServerConnection(EventEvaluation listener){
         try {
             socket = new Socket("localhost",25001);
+            DESEncode cEnc = new DESEncode();
+            cEnc.initializeCipher();
+            out = new TransmitProtocol(socket.getOutputStream(), cEnc);
+            DESDecode cDec = new DESDecode();
+            cDec.initializeCipher();
+            in = new ReceiveProtocol(socket.getInputStream(), cDec); 
+        } catch (IOException ex) {
+            Logger.getLogger(ServerConnection.class.getName()).log(Level.SEVERE, null, ex);
         }
-        catch (Exception e){
-            System.out.println("Couldn't connect to server");
-        }
+            
+            
             
     }
     
     @Override public void run(){
         while (true) {
-            String message ="";
-            try {
-                message = CypheredReadWrite.readDES(socket.getInputStream(),"01234567");
-            } catch (Exception ex) {
-                Logger.getLogger(ServerConnection.class.getName()).log(Level.SEVERE, null, ex);
+            StringBuffer message = new StringBuffer();
+            StringBuffer messageType = new StringBuffer();
+            in.readMessage(message, messageType);
+            if ("JSON".equals(messageType.toString())) {
+                JsonReader jsonReader = Json.createReader( new StringReader(message.toString()));
+                JsonObject jsonData = jsonReader.readObject();
+                for (DataReceived li1:listener) li1.dataReceived(jsonData);
             }
-            JsonReader jsonReader = Json.createReader( new StringReader(message));
-            JsonObject jsonData = jsonReader.readObject();
-            for (DataReceived li1:listener) li1.dataReceived(jsonData);
+            
         }
     }
     
-    public void transmitData(Long[] code, Double[] value){
-        Date currentTime = new Date();
-        SimpleDateFormat formatter = new SimpleDateFormat ("yyyy.MM.dd 'at' HH:mm:ss.SSS ");
-        
-        int i;
-        JsonObject transmit;
-        JsonObjectBuilder message = Json.createObjectBuilder();
-        JsonArrayBuilder dataCodes = Json.createArrayBuilder();
-        JsonArrayBuilder dataValues = Json.createArrayBuilder();
-        for (i = 0; i<code.length; i++){
-            dataCodes.add(Long.toHexString(code[i]));
-            dataValues.add(formatter.format(currentTime));
-        } 
-        message.add("Function-Name", "transmitData");
-        message.add("Data-Points",dataCodes);
-        message.add("Data-Values",dataValues);
-        transmit = message.build();
-        System.out.println(transmit.toString());
-        try {
-            CypheredReadWrite.writeDES(transmit.toString(), socket.getOutputStream(), "01234567");
-        } catch (IOException ex) {
-            Logger.getLogger(ServerConnection.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (Exception ex) {
-            Logger.getLogger(ServerConnection.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-    
-    public void close(){
-        try {
-            this.interrupt();
-            socket.close();
-        } catch (IOException ex) {
-            Logger.getLogger(ServerConnection.class.getName()).log(Level.SEVERE, null, ex);
-        }
+    public void transmitMessage(byte[] message){
+        out.writeMessage(message, "JSON".getBytes());
+            
     }
     
 }   
--- src/v2gcommunication/vehicle/VehicleMain.java
+++ src/v2gcommunication/vehicle/VehicleMain.java
@@ -4,8 +4,12 @@
  * and open the template in the editor.
  */
 package v2gcommunication.vehicle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
 
 /**
  *
@@ -13,27 +17,26 @@ import java.util.logging.Logger;
  */
 public class VehicleMain{
     public static void main(String[] args) {
-        VehicleSimulator vehicle1 = new VehicleSimulator("BMW 520d", 0.28, 0.85, 1700.0);
-        vehicle1.start();
         EventEvaluation eventEval= new EventEvaluation();
         ServerConnection conn = new ServerConnection(eventEval);
         int i = 0;
         conn.start();
-        while (i<50000){
-           /*try {
-                Thread.sleep(1);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(VehicleMain.class.getName()).log(Level.SEVERE, null, ex);
-            }
-                   */
-            Double[] speed = new Double[1];
-            speed[0] = vehicle1.getSpeed();
-            Long[] code = new Long[1];
-            code[0] = (long)i;
-            conn.transmitData(code, speed);
+        while (i<1){
+            Date currentTime = new Date();
+            SimpleDateFormat formatter = new SimpleDateFormat ("yyyy.MM.dd 'at' HH:mm:ss.SSS");
+            JsonObject transmit;
+            JsonObjectBuilder message = Json.createObjectBuilder();
+            JsonArrayBuilder dataCodes = Json.createArrayBuilder();
+            JsonArrayBuilder dataValues = Json.createArrayBuilder();
+            dataCodes.add(Integer.toHexString(i));
+            dataValues.add(formatter.format(currentTime));
+            message.add("Function-Name", "transmitData");
+            message.add("Data-Points",dataCodes);
+            message.add("Data-Values",dataValues);
+            transmit = message.build();
+            System.out.println(transmit.toString());
+            conn.transmitMessage(transmit.toString().getBytes());
             i++;
         }
-        conn.close();
-        vehicle1.destroy();
     }
 }
